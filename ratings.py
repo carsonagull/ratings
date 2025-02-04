cad1cc9baa77ff87d615417bffe80a77eaa37c5f
John Carson <120414290+carsonagull@users.noreply.github.com>
2025-02-04 12:07:39 +0300

    Add files via upload

---
+++ ratings.py
@@ -0,0 +1,4 @@
+hours= float(input("enter the number of hours worked: "))
+rate_per_hour= float(input("enter the rate per hour"))
+gross_pay=  hours* rate_per_hour
+print(f"gross pay: ${gross_pay:.2f}")
